0e17704ba8955c67617162eb42f62b366d5d0f89
BitVoyage <zhanglusheng@outlook.com>
2016-03-19 23:41:03 +0800

    start static aspects

---
+++ opinion_mining/static_aspects_extract.py
@@ -0,0 +1,9 @@
+class StaticAspectsExtract:
+    """
+    aspect label: 1 food, 2 service, 3 value, 4 decor, 5 other
+    classifier is binary, just determine be or not be certain aspect, so we need train 4 classifiers
+    """
+
+    def __init__(self):
+        pass
+
--- test/test_stanford_dependency.py
+++ test/test_stanford_dependency.py
@@ -4,15 +4,15 @@ PATH = '../extern/stanford-parser-full-2015-12-09/'
 os.environ['STANFORD_PARSER'] =PATH + 'stanford-parser.jar'
 os.environ['STANFORD_MODELS'] =PATH + 'stanford-parser-3.6.0-models.jar'
 
-#parser = stanford.StanfordParser(model_path=PATH + "englishPCFG.ser.gz")
-#sentences = parser.raw_parse_sents(("Hello, My name is Melroy.", "What is your nosetraceme?"))
-#print sentences
+# parser = stanford.StanfordParser(model_path=PATH + "englishPCFG.ser.gz")
+# sentences = parser.raw_parse_sents(("Hello, My name is Melroy.", "What is your nosetraceme?"))
+# print sentences
 #
-#for line in sentences:
-#    for sentence in line:
+# for line in sentences:
+#     for sentence in line:
 #        print sentence
 
-#dependency = stanford.StanfordDependencyParser(model_path=PATH + "englishPCFG.ser.gz")
+# dependency = stanford.StanfordDependencyParser(model_path=PATH + "englishPCFG.ser.gz")
 dep_parser=stanford.StanfordDependencyParser(model_path=PATH + "englishPCFG.ser.gz")
 sent1 = "The quick brown foxes don't like the lazy dog which are not tall."
 sent = "it's not so bad"
